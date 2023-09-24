0224d10d2746b1e6054216c96b2c5ceb0415ae9d
Shalom Michael <shalomzymike@gmail.com>
2023-09-24 19:55:26 +0100

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,17 +4,15 @@ include stdlib
 
 
 file_line { 'Turn off passwd auth':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
-  match   => '^PasswordAuthentication',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
   replace => true,
 }
 
-file_line { 'Declare identify file':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentifyFile ~/.ssh/school',
-  match   => '^IdentifyFile',
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
   replace => true,
 }
