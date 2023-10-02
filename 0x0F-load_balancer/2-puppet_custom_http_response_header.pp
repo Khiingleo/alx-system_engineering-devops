c3406e2f82f26a4baebccfae8e2dfb2637e46757
Shalom Ogoziem <shalomzymike@gmail.com>
2023-10-02 15:53:15 +0000

    puppet nginx header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,33 @@
 # automate the task of creating a custom HTTP header response, but with Puppet.
 
-$host = $::hostname
+$host = $facts['networking']['hostname']
 
 exec {'update':
   command  => 'sudo apt-get -y update',
   provider => shell,
 }
 
-exec {'install nginx',
+exec {'install nginx':
   command  => 'sudo apt-get -y install nginx',
   provider => shell,
   require  => Exec['update'],
 }
 
-package {'nginx',
+package {'nginx':
   ensure  => 'installed',
 }
 
-service {'nginx',
+service {'nginx':
   ensure  => 'running',
   enable  => true,
   require => Package['nginx'],
 }
 
-file {'/etc/nginx/sites-available/default',
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  require => Package['nginx'],
-  match   => 'listen 80 default_server',
-  line    => "\n\tadd_header X-Served-By \"${host}\";",
+file {'/etc/nginx/sites-available/default':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  match  => 'listen 80 default_server',
+  line   => "\n\tadd_header X-Served-By \"${host}\";",
 }
 
 exec {'restart service':
