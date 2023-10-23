f3d3c8ea3bb9866998cc8a1537a9aa3415b7ee0c
Shalom Ogoziem <shalomzymike@gmail.com>
2023-10-23 08:58:28 +0000

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+python script that uses REST APi for given employee ID,
+and returns info about his/her TODO list progress
+"""
+from sys import argv
+import requests
+
+
+def show_todo():
+    """displays todo list progress"""
+    base_url = "https://jsonplaceholder.typicode.com"
+    output = "Employee {} is done with tasks({}/{})"
+    user = requests.get(base_url + "/users")
+    for per in user.json():
+        if per.get("id") == int(argv[1]):
+            EMPLOYEE_NAME = (per.get("name"))
+            break
+
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    TASK_TITLE = []
+
+    to_do = requests.get(base_url + "/todos")
+    for task in to_do.json():
+        if task.get("userId") == int(argv[1]):
+            TOTAL_NUMBER_OF_TASKS += 1
+            if task.get("completed") is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(task.get("title"))
+
+    print(output.format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
+                        TOTAL_NUMBER_OF_TASKS))
+
+    for t in TASK_TITLE:
+        print("\t {}".format(t))
+
+
+if __name__ == "__main__":
+    show_todo()
